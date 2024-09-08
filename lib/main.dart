2c91055bba1b740e12c3218ce50ae2213a03247e
Youssif10 <youssifhamed10@gmail.com>
2024-09-08 10:17:11 +0300

    created first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/views/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: ('D-14 : Bottom Sheet'),
+      home: HomePage(),
+    );
+  }
+}
